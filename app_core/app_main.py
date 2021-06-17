85af5ec98c49932269e4b34f45351d84c3825b25
Filipe <filipebfisica@gmail.com>
2021-06-17 08:55:41 -0300

    templates inside frontend

--- app_core/app_main.py
+++ app_core/app_main.py
@@ -1,9 +1,12 @@
+import os
 from flask import Flask
 from app_core import frontend
 from flask_sqlalchemy import SQLAlchemy
 
 
-app = Flask(__name__)
+template_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'frontend', 'templates')  # Put this outside
+
+app = Flask(__name__, template_folder=template_dir)
 app.config.from_object('config')
 frontend.init_app(app)
 
